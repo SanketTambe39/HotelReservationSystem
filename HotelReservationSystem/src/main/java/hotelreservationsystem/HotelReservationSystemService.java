f26f8fc71f0d98344ce5afaed9899ec6c776ddd6
SanketTambe39 <sankettambe39@gmail.com>
2021-07-20 11:12:20 +0530

    ADDED: ratings to each hotel

--- HotelReservationSystem/src/main/java/hotelreservationsystem/HotelReservationSystemService.java
+++ HotelReservationSystem/src/main/java/hotelreservationsystem/HotelReservationSystemService.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 
 public interface HotelReservationSystemService {
 
-	 public void addHotelDetails(String hotelName, int weekDayRate, int weekendRate);
+	 public void addHotelDetails(String hotelName, int weekDayRate, int weekendRate,int rating);
 	 public ArrayList<String> findCheapestHotelForRegularCustomer(String arrival, String checkout);
 }
